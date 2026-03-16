5b7b5d1ec8b49741a2ff4aa84dbdaa35b2f81cee
NarasimhaRao41 <143867308+NarasimhaRao41@users.noreply.github.com>
2026-03-16 18:02:58 +0530

    LinkedListCycle using Slow and Fast Two Pointer Approach
    Added examples and explanations for linked list cycle detection.

---
+++ 141_LinkedListCycle.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next== null){
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast ){
+            return true;
+        }
+     }
+        return false;
+    }
+}
+
+
+Examples :
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
